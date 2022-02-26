af15f8d1515b13bf6cd77c8d8c907c598bc291cf
aliaflore <100151385+aliaflore@users.noreply.github.com>
2022-02-26 18:04:42 +0200

    prb 2 e aproape gata

--- lab 1/ex 2.cpp
+++ lab 1/ex 2.cpp
@@ -1,20 +1,35 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 using namespace std ;
 
 int main ()
 {
-    FILE * fp ;
-    
-    if ( fopen_s ( &fp , "in.txt" , "r" ) != 0 )
+    int n , suma = 0 ;
+    FILE* fp;
+    fp = fopen ( "in.txt" , "r" ) ;
+	if ( fp == 0 )
+	{
+		perror ( "Eroare la deschidere, cu mesajul de eroare:" ) ;
+        exit ( 1 ) ;
+	}
+	else 
     {
-        printf ( "Eroare: nu am putut deschide fisierul\n" ) ;
+		//printf ( "Am deschis fisierul cu success!\n" ) ;
+		char myString [ 200 ] ;
+		while ( fgets ( myString , 200 , fp ) != 0 )
+		{
+			myString [ strlen ( myString ) - 1 ] = '\0' ;
+			printf ( "%s\n" , myString ) ;
+			n = 0 ;
+			for ( int i = 0 ; i < strlen ( myString ) ; i ++ )
+				n = n * 10 + ( myString [ i ] - 48 );
+			printf ( "%d\n" , n ) ;
+			suma += n ;
+		}
+		printf ( "%d\n" , suma ) ;
+
     }
-    else 
-    {
-        printf ( "Am deschis fisierul cu succes!" ) ;
-    }
-    system ( "pause ") ;
     return 0 ;
 }
